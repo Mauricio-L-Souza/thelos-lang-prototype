cdad61acd31713a0acdebb82ee44fdf9c4d7a556
mauricio souza <newmailmlcs@gmail.com>
2025-01-06 22:42:46 -0300

    IMPROVED: builtin

--- core/builtins/outputln.builtin.js
+++ core/builtins/outputln.builtin.js
@@ -4,6 +4,14 @@ class OutputLn extends Builtin {
     typo = 'outputln';
 
     perform(str) {
+        let isArray = Array.isArray(str);
+
+        if (isArray && str.length > 1) {
+            throw new Error('ExecutionError: builtin outputln function expects only 1 arg, ' + str.length + ' passed.');
+        }
+
+        if (isArray) str = str[0];
+
         process.stdout.write(str + "\n");
     }
 }
--- core/globals/utils.js
+++ core/globals/utils.js
@@ -1,3 +1,5 @@
+const Program = require("./program");
+
 function starts_with_letter(str) {
     const charCode = str.charCodeAt(0);
     return (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122);
@@ -15,7 +17,22 @@ function has_special_charactere(str) {
     return false;
 }
 
+/**
+ * 
+ * @param {Program} program
+*/
+function prepare_value(program, value) {
+    if (value.charAt(0) === "\"") {
+        return JSON.parse(value);
+    } else if (!Number.isNaN(Number(value))) {
+        return Number(value);
+    } else if (program.recoverVariable(value)) {
+        return prepare_value(program, program.recoverVariable(value));
+    }
+}
+
 module.exports = {
     has_special_charactere,
-    starts_with_letter
+    starts_with_letter,
+    prepare_value
 };
