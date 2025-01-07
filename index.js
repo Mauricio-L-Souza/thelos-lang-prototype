945f045168461420f23fc3e49f1444de6a998f32
mauricio souza <newmailmlcs@gmail.com>
2025-01-06 22:41:52 -0300

    REMOVED: comments

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// function inputscan() {
-//     process.stdin.resume();
-//     return new Promise((resolve, reject) => {
-//         process.stdin.on('data', (data) => {
-//             process.stdin.pause();
-//             resolve(data.toString().trim());
-//         });
-//     });
-// }
-
-// async function main() {
-//     process.stdout.write('>> ');
-//     let value = await inputscan();
-//     console.log({ value });
-// }
-
-// variable $= main => "asdasd", "asdasd", "asdasd";
-
 const Scope = require('./core/globals/scope');
 const generator = require('./core/parser');
 const tokenizer = require('./core/tokenizer');
